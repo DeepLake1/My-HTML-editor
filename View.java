8817339ddd99a8e614a79657ea4f2fa9eed31bd0
DeepLake1 <saawkkaa@gmail.com>
2019-04-13 23:54:46 +0300

    some changes

--- View.java
+++ View.java
@@ -2,8 +2,12 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,7 +17,10 @@ public class View extends JFrame implements ActionListener {
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
-    public View(){
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+    public View() {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (IllegalAccessException e) {
@@ -30,10 +37,31 @@ public class View extends JFrame implements ActionListener {
         }
 
     }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
+
+    public void undo() {
+       try { undoManager.undo();}
+       catch (CannotUndoException undoExeption){
+           ExceptionHandler.log(undoExeption);
+       }
+
+    }
+
+    public void redo() {
+        try { undoManager.redo();}
+        catch (CannotRedoException undoExeption){
+            ExceptionHandler.log(undoExeption);
+        }
+    }
+    public void resetUndo(){undoManager.discardAllEdits();}
 
     public Controller getController() {
         return controller;
@@ -60,14 +88,14 @@ public class View extends JFrame implements ActionListener {
         //Создаём панель менюхи
 
         JMenuBar jMenuBar = new JMenuBar();
-        MenuHelper.initFileMenu(this,jMenuBar);
-        MenuHelper.initEditMenu(this,jMenuBar);
-        MenuHelper.initStyleMenu(this,jMenuBar);
-        MenuHelper.initAlignMenu(this,jMenuBar);
-        MenuHelper.initColorMenu(this,jMenuBar);
-        MenuHelper.initFontMenu(this,jMenuBar);
-        MenuHelper.initHelpMenu(this,jMenuBar);
-        getContentPane().add(jMenuBar,BorderLayout.NORTH);
+        MenuHelper.initFileMenu(this, jMenuBar);
+        MenuHelper.initEditMenu(this, jMenuBar);
+        MenuHelper.initStyleMenu(this, jMenuBar);
+        MenuHelper.initAlignMenu(this, jMenuBar);
+        MenuHelper.initColorMenu(this, jMenuBar);
+        MenuHelper.initFontMenu(this, jMenuBar);
+        MenuHelper.initHelpMenu(this, jMenuBar);
+        getContentPane().add(jMenuBar, BorderLayout.NORTH);
 
     }
 
@@ -77,11 +105,10 @@ public class View extends JFrame implements ActionListener {
         tabbedPane.add("HTML", jScrollPaneHtml);
         JScrollPane jScrollPaneText = new JScrollPane(plainTextPane);
         tabbedPane.add("Текст", jScrollPaneText);
-        tabbedPane.setPreferredSize(new Dimension(500,250));
+        tabbedPane.setPreferredSize(new Dimension(500, 250));
         TabbedPaneChangeListener tabbedPaneChangeListener = new TabbedPaneChangeListener(this);
         tabbedPane.addChangeListener(tabbedPaneChangeListener);
-        getContentPane().add(tabbedPane,BorderLayout.CENTER);
-
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
 
 
     }
@@ -95,5 +122,14 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void selectedTabChanged() {
+    }
+
+    public boolean canUndo() {
+        return undoManager.canUndo();
+    }
+
+    public boolean canRedo() {
+        return undoManager.canRedo();
+
     }
 }
